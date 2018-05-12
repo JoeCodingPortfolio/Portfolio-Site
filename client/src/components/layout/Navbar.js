837cb0e0e8901b5dc25f18da003a9d4b2d7d5e14
joey <joe1995@protonmail.com>
2018-05-12 11:28:47 +0100

    Added The Signup And Login

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -6,9 +6,9 @@ class Navbar extends Component {
     return (
       <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
         <div className="container">
-          <a className="navbar-brand" href="landing.html">
+          <Link className="navbar-brand" to="/">
             Joe's Coding Portfolio
-          </a>
+          </Link>
           <button
             className="navbar-toggler"
             type="button"
@@ -21,10 +21,10 @@ class Navbar extends Component {
           <div className="collapse navbar-collapse" id="mobile-nav">
             <ul className="navbar-nav mr-auto">
               <li className="nav-item">
-                <a className="nav-link" href="profiles.html">
+                <Link className="nav-link" to="/profiles">
                   {" "}
-                  My Websites
-                </a>
+                  My Projects
+                </Link>
               </li>
             </ul>
 
